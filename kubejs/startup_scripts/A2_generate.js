1cad02e2152756438581445408a08cc120c069ac
regaccio <e@mail.address>
2026-04-23 20:39:19 -0400

    chapters: deepslate gen, register liquid soul

--- kubejs/startup_scripts/A2_generate.js
+++ kubejs/startup_scripts/A2_generate.js
@@ -3,3 +3,9 @@ StartupEvents.registry("item", event => {
     event.create("rubber").texture("a2:item/rubber").displayName("Rubber")
     event.create("raw_rubber").texture("a2:item/raw_rubber").displayName("Raw Rubber")
 })
+
+StartupEvents.registry("fluid", event => {
+    // 1.21: tconstruct liquid soul replacement
+    //placeholder texture
+    event.create("liquid_soul").displayName("Liquid Soul").stillTexture("oritech:block/fluid/fluid_roiling_plasma").flowingTexture("oritech:block/fluid/fluid_roiling_plasma")
+})
