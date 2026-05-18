2462139ce71ffbfe78e28fb4e86e62da6c4ff98d
Lint Action <lint-action@samuelmeuli.com>
2026-05-18 04:56:35 +0000

    Fix code style issues with ESLint

--- kubejs/server_scripts/potions.js
+++ kubejs/server_scripts/potions.js
@@ -3,7 +3,7 @@ if (typeof MoreJS !== "undefined" && MoreJS.registerPotionBrewing) {
     MoreJS.registerPotionBrewing((event) => {
         // Slow-falling potion that doesn't require ph*nt*ms
         event.addPotionBrewing(Ingredient.of("minecraft:wind_charge"), "minecraft:awkward", "minecraft:slow_falling");
-        
+
         // Haste potion (chapter 2b)
         // removePotionBrewing is broken, used datapack to delete duplicates:
         // apothic_attributes, galosphere
@@ -11,7 +11,7 @@ if (typeof MoreJS !== "undefined" && MoreJS.registerPotionBrewing) {
         event.addPotionBrewing(Ingredient.of("minecraft:glow_berries"), "minecraft:awkward", "alexscaves:haste");
         event.addPotionBrewing(Ingredient.of("minecraft:redstone"), "kubejs:haste", "alexscaves:long_haste");
         event.addPotionBrewing(Ingredient.of("minecraft:glowstone_dust"), "kubejs:haste", "alexscaves:strong_haste");
-        
+
         // New Potion of Glowing recipe: brewing Glow Lichen in an Awkward Potion.
         event.addPotionBrewing(Ingredient.of("minecraft:glow_lichen"), "minecraft:awkward", "alexscaves:glowing")
     })
