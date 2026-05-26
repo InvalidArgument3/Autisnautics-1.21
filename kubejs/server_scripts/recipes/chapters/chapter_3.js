2d06c5b7fbc5767fdaa1b8173a479fa6d0e302f4
Lint Action <lint-action@samuelmeuli.com>
2026-05-26 06:20:47 +0000

    Fix code style issues with ESLint

--- kubejs/server_scripts/recipes/chapters/chapter_3.js
+++ kubejs/server_scripts/recipes/chapters/chapter_3.js
@@ -3,7 +3,7 @@
 ServerEvents.recipes(event => {
     // --- Chapter 3
     let transitional
-    
+
     // -- Farming Ender Dust from Snow Golems
     // - Grinding Haunted Rind into Haunted Dust
     event.recipes.create.milling(["kubejs:haunted_dust"], "kubejs:haunted_rind")
@@ -19,10 +19,10 @@ ServerEvents.recipes(event => {
         },
         "secondaries": [
             {
-            "chance": 0.25,
-            "output": {
-                "item": "kubejs:haunted_dust"
-            }
+                "chance": 0.25,
+                "output": {
+                    "item": "kubejs:haunted_dust"
+                }
             }
         ]
     })
@@ -30,18 +30,18 @@ ServerEvents.recipes(event => {
         "type": "nuclearcraftneohaul:manufactory_recipe",
         "itemIngredients": [
             {
-            "count": 1,
-            "ingredient": {
-                "item": "kubejs:haunted_rind"
-            }
+                "count": 1,
+                "ingredient": {
+                    "item": "kubejs:haunted_rind"
+                }
             }
         ],
         "itemProducts": [
             {
-            "count": 1,
-            "ingredient": {
-                "item": "kubejs:haunted_dust"
-            }
+                "count": 1,
+                "ingredient": {
+                    "item": "kubejs:haunted_dust"
+                }
             }
         ],
         "powerModifier": 1.0,
@@ -50,7 +50,7 @@ ServerEvents.recipes(event => {
     })
     // - Bulk Washing Haunted Dust to Ender Dust (can also be obtained through laser alchemy)
     event.recipes.create.splashing(["ae2:ender_dust"], "kubejs:haunted_dust")
-    
+
     // -- Gunpowder Production
     // - Remove unwanted sources
     event.remove({ id: "galosphere:gunpowder_from_pink_salt_shard" })
@@ -59,28 +59,28 @@ ServerEvents.recipes(event => {
     event.remove({ id: "create:filling/gunpowder" })
     // - CDG Diesel as a sulfur source - simpler than IE high pressure refiner but inferior
     // Lubricant -> Sulfurized Diesel + Paraffin Wax
-    event.recipes.create.mixing([Fluid.of("immersivepetroleum:diesel_sulfur", 800), CreateItem.of("immersivepetroleum:paraffin_wax", 0.75)], 
-                                [Fluid.sizedIngredientOf("immersivepetroleum:lubricant", 1000), 
-                                Fluid.sizedIngredientOf("minecraft:water", 500)])
-                                .superheated()
+    event.recipes.create.mixing([Fluid.of("immersivepetroleum:diesel_sulfur", 800), CreateItem.of("immersivepetroleum:paraffin_wax", 0.75)],
+        [Fluid.sizedIngredientOf("immersivepetroleum:lubricant", 1000),
+            Fluid.sizedIngredientOf("minecraft:water", 500)])
+        .superheated()
     // Sulfurized Diesel -> CDG Diesel + Sulfur
     event.recipes.create.mixing([Fluid.of("createdieselgenerators:diesel", 800),
-                                Item.of("immersiveengineering:dust_sulfur", 3),
-                                CreateItem.of("immersiveengineering:dust_sulfur", 0.75)],
-                                [Fluid.sizedIngredientOf("immersivepetroleum:diesel_sulfur", 1000),
-                                Fluid.sizedIngredientOf("minecraft:water", 500)])
-                                .superheated()
+        Item.of("immersiveengineering:dust_sulfur", 3),
+        CreateItem.of("immersiveengineering:dust_sulfur", 0.75)],
+    [Fluid.sizedIngredientOf("immersivepetroleum:diesel_sulfur", 1000),
+        Fluid.sizedIngredientOf("minecraft:water", 500)])
+        .superheated()
     // - Galosphere Pink Salt stuff as a nitrate source
     event.recipes.create.milling([Item.of("immersiveengineering:dust_saltpeter", 2)], "galosphere:pink_salt_shard")
-    event.recipes.create.crushing(["immersiveengineering:dust_saltpeter", CreateItem.of("immersiveengineering:dust_saltpeter", 0.5)], 
-                                "galosphere:pink_salt")
-    event.recipes.create.crushing(["immersiveengineering:dust_saltpeter", CreateItem.of("immersiveengineering:dust_saltpeter", 0.5)], 
-                                "galosphere:rose_pink_salt")
-    event.recipes.create.crushing(["immersiveengineering:dust_saltpeter", CreateItem.of("immersiveengineering:dust_saltpeter", 0.5)], 
-                                "galosphere:pastel_pink_salt")
+    event.recipes.create.crushing(["immersiveengineering:dust_saltpeter", CreateItem.of("immersiveengineering:dust_saltpeter", 0.5)],
+        "galosphere:pink_salt")
+    event.recipes.create.crushing(["immersiveengineering:dust_saltpeter", CreateItem.of("immersiveengineering:dust_saltpeter", 0.5)],
+        "galosphere:rose_pink_salt")
+    event.recipes.create.crushing(["immersiveengineering:dust_saltpeter", CreateItem.of("immersiveengineering:dust_saltpeter", 0.5)],
+        "galosphere:pastel_pink_salt")
     // - Small nitrate byproduct from crushing dripstone block
     event.recipes.create.crushing(["minecraft:clay_ball", CreateItem.of("immersiveengineering:dust_saltpeter", 0.25)], "minecraft:dripstone_block")
-    
+
     // -- Singularity Production
     // - Crushing Wheels: simpler recipe
     event.remove({ id: "create:mechanical_crafting/crushing_wheel" })
@@ -96,7 +96,7 @@ ServerEvents.recipes(event => {
     })
     // - Crushing the Crushing Wheel in the Crushing Wheel...
     event.recipes.create.crushing([Item.of("ae2:singularity")], "create:crushing_wheel").processingTime(250)
-    
+
     // -- Chromatic Singularities
     let chromaticColors = colours.slice(1).filter((entry) => (entry.includes("gray") || entry.includes("black")) == false ) // only colorful dyes allowed
     chromaticColors.forEach(color => {
@@ -106,10 +106,10 @@ ServerEvents.recipes(event => {
     if (Platform.isLoaded("create_dragons_plus")) {
         chromaticColors.forEach(color => {
             event.recipes.create.compacting([Item.of("kubejs:dye_entangled_singularity", 1), Fluid.of("minecraft:water", 125)],
-                                            [Fluid.sizedIngredientOf(`create_dragons_plus:${color}_dye`, 125), "ae2:quantum_entangled_singularity"])
+                [Fluid.sizedIngredientOf(`create_dragons_plus:${color}_dye`, 125), "ae2:quantum_entangled_singularity"])
         })
     }
-    
+
     // -- Paint Balls and Color Correction
     event.remove({ id: /ae2:tools\/paintballs.*/ })
     event.recipes.create.crushing([
@@ -127,7 +127,7 @@ ServerEvents.recipes(event => {
             continue;
         event.recipes.create.emptying([`ae2:${spectrum[i + 1]}_paint_ball`, Fluid.of("supplementaries:lumisene", 100)], `ae2:${color}_paint_ball`)
     }
-    
+
     // -- Chromatic Compound
     event.recipes.create.mechanical_crafting("create:chromatic_compound", [
         "AA",
@@ -140,30 +140,30 @@ ServerEvents.recipes(event => {
         "type": "nuclearcraftneohaul:assembler_recipe",
         "itemIngredients": [
             {
-            "count": 4,
-            "ingredient": {
-                "item": "ae2:magenta_paint_ball"
-            }
+                "count": 4,
+                "ingredient": {
+                    "item": "ae2:magenta_paint_ball"
+                }
             }
         ],
         "itemProducts": [
             {
-            "count": 1,
-            "ingredient": {
-                "item": "create:chromatic_compound"
-            }
+                "count": 1,
+                "ingredient": {
+                    "item": "create:chromatic_compound"
+                }
             }
         ],
         "powerModifier": 1.0,
         "radiation": 0.0,
         "timeModifier": 1.0
     })
-    
-    
+
+
     // -- Refined Radiance: Create is bugged so this can't be made without a beacon, temporary fix below (EntityEvents.spawned for dropped items)
     // - besides burning lumisene waste, you can manufacture torches from trees, deploy bonemeal on glow lichen, etc...
     // todo: mysterious conversion/information entry/tooltip etc
-    
+
     // -- Radiant Coils
     event.recipes.create.pressing("kubejs:radiant_sheet", "create:refined_radiance")
     event.recipes.create.mechanical_crafting("kubejs:radiant_coil", ["A"], { A: "kubejs:radiant_sheet" })
@@ -172,25 +172,25 @@ ServerEvents.recipes(event => {
         "type": "nuclearcraftneohaul:assembler_recipe",
         "itemIngredients": [
             {
-            "count": 1,
-            "ingredient": {
-                "item": "kubejs:radiant_sheet"
-            }
+                "count": 1,
+                "ingredient": {
+                    "item": "kubejs:radiant_sheet"
+                }
             }
         ],
         "itemProducts": [
             {
-            "count": 1,
-            "ingredient": {
-                "item": "kubejs:radiant_coil"
-            }
+                "count": 1,
+                "ingredient": {
+                    "item": "kubejs:radiant_coil"
+                }
             }
         ],
         "powerModifier": 1.0,
         "radiation": 0.0,
         "timeModifier": 0.5
     })
-    
+
     // -- Chromatic Resonator
     // - normal recipe, using powergrid:magnet
     event.shaped("kubejs:chromatic_resonator", [
@@ -211,7 +211,7 @@ ServerEvents.recipes(event => {
         L: Ingredient.of("#c:ingots/lead"),
         B: "alexscaves:azure_neodymium_ingot"
     })
-    
+
     // -- Inductive Mechanisms
     transitional = "kubejs:incomplete_inductive_mechanism"
     event.recipes.create.sequenced_assembly([
@@ -223,10 +223,10 @@ ServerEvents.recipes(event => {
     ]).transitionalItem(transitional)
         .loops(1)
         .id("kubejs:inductive_mechanism")
-    
+
     // -- Constantan Casing
     event.shapeless(Item.of("kubejs:constantan_casing", 2), [Ingredient.of("#c:plates/constantan"), "immersiveengineering:sheetmetal_steel"])
-    
+
     // -- Constantan Machines
     donutCraft(event, "kubejs:constantan_machine", "kubejs:constantan_casing", "kubejs:inductive_mechanism")
     // - Usages
